cce3799baa73b7c57f4654285c82087b88926b6b
SITE0350A4 <kristapeterson144@gmail.com>
2018-07-17 15:14:58 -0700

    Data project

--- collision data.py
+++ collision data.py
@@ -1,11 +1,50 @@
 import csv
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+data=[[],[],[]]
+class Accidents:
+    def __init__(self, time, injury, death):
+        self.time=time
+        self.injury=injury
+        self.death=death
+
+
+
 def load_file (file_name):
+    global data
+    line_number = 0
     with open (file_name) as a:
         for line in a :
-            # line=line.replace (''',' ')
-            for word in (line.split(',')):
-                print (word, end='')
-    a.close
+            try:
+                collision_time=line.split(',')[1].split()[1]
+                collision_injury=int(line.split(',')[13].split())
+                collision_death=int(line.split(',')[14].split())
+                line_number+=1
+                print (line_number, collision_time, collision_injury, collision_death)
+                if collision_time=='00:01:00"':
+                    continue
+                data[0].append(int(collision_time[:2]))
+                if collision_injury>0:
+                    data[1].append(collision_time[:2])
+                if collision_injury>0:
+                    data[2].append(collision_time[:2])
+            except:
+                pass
+
+    a.close()
 
 if __name__=="__main__":
     load_file('collisions')
+    print (data)
+    num_bins = 24
+    fig, axes = plt.subplots()
+    colors = ['red', 'tan', 'lime']
+
+    n, bins, patches = axes.hist(data, num_bins, alpha=0.5, color=colors, label=colors)
+
+    axes.legend(prop={'size': 10})
+    axes.set_title('bars with legend')
+    plt.show()
+
+
