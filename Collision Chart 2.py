75548d4b1a85a20581fba88d925566bbff98f7ea
SITE0350A4 <kristapeterson144@gmail.com>
2018-07-19 16:24:26 -0700

    Data project

--- Collision Chart 2.py
+++ Collision Chart 2.py
@@ -1,4 +1,6 @@
 import csv
+from collections import Counter
+
 import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
@@ -32,24 +34,32 @@ def load_file (file_name):
             except:
                 pass
     a.close()
+
 if __name__=="__main__":
     load_file('collisions')
-    data=[np.array(accident),np.array(injury),np.array(death)]
+    a=Counter(accident)
+    i=Counter(injury)
+    d=Counter(death)
+    data=[[],[]]
+    for hour in range(24):
+        print(i[hour]/a[hour])
+        print(d[hour]/a[hour])
+        if (i[hour]/a[hour])> 0:
+            data[0].append(i[hour]/a[hour])
+        if (d[hour]/a[hour])> 0:
+            data[1].append(d[hour]/a[hour])
+    # data=[np.array(injury),np.array(death)]
     print (data)
     num_bins = np.arange(25)-.5
-    colors = ['blue', 'red', 'green']
+    colors = ['red', 'green']
     fig, ax1 = plt.subplots()
-    ax1.hist([data[0], data[1],np.empty(1)], num_bins,  range=(0,23), color=colors, label=['Accidents', 'Injuries', 'Deaths'])
-    ax2=ax1.twinx()
-    ax2.hist([np.empty(1), np.empty(1),data[2]], num_bins, range=(0,23), color=colors, label=['Accidents', 'Injuries', 'Deaths'])
+    ax1.bar(range(24), data[0], color='red', width=.4)
+    ax1.bar(np.arange(.5,24), data[1], color='green', width=.4)
     ax1.xaxis.set_major_formatter(FormatStrFormatter('%02d:00'))
     ax1.set(xticks=range(0,24,4))
+    ax1.set(yticks=np.arange(0,1.1,.2))
+    plt.gca().set_yticklabels(['{:.0f}%'.format(x * 100) for x in plt.gca().get_yticks()])
     ax1.set_ylabel('Accidents & Injuries')
-    ax2.set_ylabel('Deaths', color='green')
-    ax1.set_xlabel('Hours of the Day')
-    ax2.tick_params('y', colors='green')
-    ax2.legend(prop={'size': 10})
-    ax2.set_title('San Diego Car Collisions')
+    ax1.legend(prop={'size': 10})
+    ax1.set_title('Percentage of Injuries and Deaths in San Diego Car Collisions')
     fig.show()
-
-
